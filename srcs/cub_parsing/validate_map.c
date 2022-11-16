6bc49d94d9525092b867e4a46e90d78a46133cfa
Leo Bounor <lbounor@z1r2p4.42lyon.fr>
2022-11-16 17:32:46 +0100

    validate map dev

--- srcs/cub_parsing/validate_map.c
+++ srcs/cub_parsing/validate_map.c
@@ -6,13 +6,20 @@
 /*   By: lbounor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:56:58 by lbounor           #+#    #+#             */
-/*   Updated: 2022/11/10 15:57:42 by lbounor          ###   ########lyon.fr   */
+/*   Updated: 2022/11/16 11:21:42 by lbounor          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-int	validate_map(t_data *data)
+static int	check_border(t_data *data)
 {
 
+}
+
+int	validate_map(t_data *data)
+{
+	if (check_border(data) == 1)
+		return (1);
+	
 }
--- srcs/cub_parsing/validate_parsing.c
+++ srcs/cub_parsing/validate_parsing.c
@@ -6,11 +6,11 @@
 /*   By: lbounor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:06:57 by lbounor           #+#    #+#             */
-/*   Updated: 2022/11/10 15:48:50 by lbounor          ###   ########lyon.fr   */
+/*   Updated: 2022/11/16 17:32:16 by lbounor          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cub3d.h>
+#include "../../includes/cub3d.h"
 
 static void	ft_exit(char *s, t_data *data)
 {
@@ -33,4 +33,41 @@ void	validate_parsing(t_data *data)
 		ft_exit("Error\nCannot load colors\n", data);
 	if (validate_map() == 1)
 		ft_exit("Error\nInvalid map\n", data);
+}
+
+static int	check_border(int i, t_data *data)
+{
+	int	j;
+
+	j = 0;
+	while (data->map_data.map[i][j] < data->map_data.width)
+	{
+		if (data->map_data.map[i][j] != 1 && data->map_data.map[i][j] != -1)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	validate_map(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (data->map_data.map[i] < data->map_data.height)
+	{
+		j = 0;
+		if (i == 0)
+			if (check_border(0, data) == 1)
+				return (1);
+		while (data->map_data.map[i][j] < data->map_data.width)
+		{
+			if (is_closed(i, j) == 1)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
